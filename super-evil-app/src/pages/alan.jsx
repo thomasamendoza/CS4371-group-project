f2997ecb1642fbbb2c484372b86e390d594854cb
Alan De La Torre <sua9@txstate.edu>
2025-04-16 09:21:53 -0500

    need to change output logs

--- super-evil-app/src/pages/alan.jsx
+++ super-evil-app/src/pages/alan.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 function IndirectPrompt() {
-    
-    return (
-        <>
-        <div className="App">
-            <div className="content-container">
-                <div className="clock-container">
-                    <p>Hi There</p>
-                </div>
-            </div>
+  return (
+    <>
+      <div className='App'>
+        <div className='content-container'>
+          <div className='clock-container'>
+            <p>Hi There</p>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default IndirectPrompt;
